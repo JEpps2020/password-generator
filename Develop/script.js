bf311a20557a69e47524bd8a0ce408f9476d5bf9
Jasmine Epps <epps.jasmine@gmail.com>
2020-02-22 20:11:26 -0500

    I have modified the function to generate the password on button click.

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,11 @@
-//create onclick function to get
+// //create onclick function to get
 var masterarray=[];
-//A.first prompt 
+// //A.first prompt
+function generatePassword(){
+
 var userLength = prompt("How many characters would you like your password to contain?")
-if (userLength <8 || userLength>128) {
-  //alert length not in range
+if (parseInt(userLength) < 8 || parseInt(userLength) >128) {
+ //alert length not in range
   alert ("This is not not a secure length.")  
  //ask again for user length
 
@@ -13,6 +15,7 @@ else {
   
   //ask for special characters
   var askSpecial;
+  var askSpecial = confirm("Do you want special characters?")
   var specialArr=["!","$","%","&","@"];
   //if they say yes
   if(askSpecial) {
@@ -20,65 +23,65 @@ else {
       for(var i=0; i<specialArr.length;i++){
         masterarray.push(specialArr[i]);
       }
-      				//masterArray = ["!","@","$"];
-				//run a for loop based on length of speical char 
-        //masterarray.push(specialchar[i]);
+
+      console.log(masterarray)
+
+      var password="";
+            
+    //I want to generate a random password based upon the choices above.
+    for(var i=0; i<userLength.length;i++){
+          
+      }
+//       				//masterArray = ["!","@","$"];
+// 				//run a for loop based on length of speical char 
+//         //masterarray.push(specialchar[i]);
    
-   //ask for numbers
-   var askNum;
-   var numArr = [1,2,3,4,5,6,7,8,9];
-   if(askNum){
-      for(var i=0; i<numArr.length;i++){
-      masterarray.push(numArr[i]);
+//    //ask for numbers
+//    var askNum;
+//    var numArr = [1,2,3,4,5,6,7,8,9];
+//    if(askNum){
+//       for(var i=0; i<numArr.length;i++){
+//       masterarray.push(numArr[i]);
 
-   }
+//    }
 
-   //ask for lower case letters
-   var lower;
-   var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-   if(lower){
-    for(var i=0; i<lowerArr.length;i++){
-      masterarray.push(lowerArr[i]);
-   }
+//    //ask for lower case letters
+//    var lower;
+//    var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//    if(lower){
+//     for(var i=0; i<lowerArr.length;i++){
+//       masterarray.push(lowerArr[i]);
+//    }
    
-   //ask for upper case letters
-   var upper;
-   var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-   if(upper){
-    for(var i=0; i<upperArr.length;i++){
-      masterarray.push(upperArr[i]);
-   }
+//    //ask for upper case letters
+//    var upper;
+//    var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//    if(upper){
+//     for(var i=0; i<upperArr.length;i++){
+//       masterarray.push(upperArr[i]);
+//    }
 
-  //we will update the masterarray based on yes or no
+// }
 
-			//B. generate passwords
-			var password="";
-			//prob create a dynamic (pwarray) array with data
-			//asked from prompt
-			//create a forloop based on length, 
-			for(var i=0;i<userlength;i++)
-			{
-				//
-				password+=(randomly selected values from masterarray)
-				//will need to use math.floor
-				//math.random
-			}
-			//call randomly based on length the masterarray
-			console.log(password);
-			
-			//C. stick to html
-			//#password
-			//document.getelementbyid("password").val(password);
 }
+
+}
+
+}
+
+//    console.log(masterarray)
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log("hello")
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // passwordText.value = password;
 
 }
 
