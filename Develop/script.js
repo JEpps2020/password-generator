01a1ebab9f8fd16d209c2a05a291185af5f21b45
Jasmine Epps <epps.jasmine@gmail.com>
2020-02-23 19:53:38 -0500

    I have added the math random function to the script.

--- Develop/script.js
+++ Develop/script.js
@@ -15,7 +15,7 @@ else {
   
   //ask for special characters
   var askSpecial;
-  var askSpecial = confirm("Do you want special characters?")
+  var askSpecial = confirm("Click ok to include special characters.")
   var specialArr=["!","$","%","&","@"];
   //if they say yes
   if(askSpecial) {
@@ -26,51 +26,79 @@ else {
 
       console.log(masterarray)
 
-      var password="";
-            
-    //I want to generate a random password based upon the choices above.
-    for(var i=0; i<userLength.length;i++){
-          
+  // ask for numeric characters
+  var askNum;
+  var askNum = confirm("Click ok to include numeric characters.")
+  var numArr = [1,2,3,4,5,6,7,8,9];
+  //if they say yes 
+  if(askNum){
+    //update masterarray 
+      for(var i=0; i<numArr.length;i++){
+      masterarray.push(numArr[i]);
       }
-//       				//masterArray = ["!","@","$"];
-// 				//run a for loop based on length of speical char 
-//         //masterarray.push(specialchar[i]);
-   
-//    //ask for numbers
-//    var askNum;
-//    var numArr = [1,2,3,4,5,6,7,8,9];
-//    if(askNum){
-//       for(var i=0; i<numArr.length;i++){
-//       masterarray.push(numArr[i]);
 
-//    }
+      console.log(masterarray)
 
-//    //ask for lower case letters
-//    var lower;
-//    var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-//    if(lower){
-//     for(var i=0; i<lowerArr.length;i++){
-//       masterarray.push(lowerArr[i]);
-//    }
-   
-//    //ask for upper case letters
-//    var upper;
-//    var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-//    if(upper){
-//     for(var i=0; i<upperArr.length;i++){
-//       masterarray.push(upperArr[i]);
-//    }
+  //ask for lower case letters
+   var lowerChar;
+   var lowerChar = confirm("Click ok to confirm lower case letters.")
+   var charArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+   if(lowerChar){
+      
+    for(var i=0; i<charArr.length;i++){
+      masterarray.push(charArr[i]);
+   }
+
+   console.log(masterarray)
+    
+  //ask for upper case letters
+   var upper;
+   var upper = confirm("Click ok to confirm upper case letters.")
+   var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+   if(upper){
+    for(var i=0; i<upperArr.length;i++){
+      masterarray.push(upperArr[i]);
+   }
+
+   console.log(masterarray)
+  
+  var newPass="";
+  var newPass = confirm("Click ok to confirm secure password.")
+  if(newPass){  
+  for(var i=0; i<userLength.length;i++){
+        newPass += Math.floor(Math.random() * (masterarray.length - 1)-0+1) + 0
+                  
+        console.log(newPass)
+    }          
+
+
+
+
+
+
+}
+
+}
+
+}
+
+}
+
+}
+
+}
+
+}
+
+
+// function password(userLength,){
+//   var password="";
+//   for (var i=0; i<userLength.length;i++){
+//   password += Math.floor(Math.random() * (masterarray.length - 1)-0+1) + 0
+// }
+// return password;
 
 // }
-
-}
-
-}
-
-}
-
-//    console.log(masterarray)
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
